68c07c4e56d37f31af590d220551c37e1b346c12
happychild8p <happychild8p@gmail.com>
2020-10-22 03:58:29 -0400

    Part1 Creating Indicator

--- prj_indicators1003.py
+++ prj_indicators1003.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
 '''
-Closed School Utilization with text mining
-Purpose: To extract useful keywords could be assist those who want to utilze closed school
-Author: Heedong Yang
-
-After crawling news
-Use news, theses and example text to
-1. Clean it: 
-2. Tokenize
-3-1. Create dtm
-3-2. Apply tfidf
-4. Apply LDA to group words
+@Authership declaration@
+@ Author: Heedong Yang
+@ Purpose: Closed school utilization projcet Part1 : Making Indicator using text analysis
+@ Date: Oct 22, 2020
+@ Usage:
 '''
-
 import pandas as pd
 import networkx as nx
-import matplotlib.pyplot as plt
 from konlpy.tag import Hannanum
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
-import pyLDAvis.sklearn
+import gensim
 from gensim.models import Word2Vec
 from apyori import apriori
 
@@ -28,7 +20,8 @@ from apyori import apriori
 def get_cleaned_text(file, doc_len=-1):
     f = open(file, 'r', encoding='utf-8')
     newsStr = f.read()
-    doclist = list(newsStr.split("|"))[0:doc_len]
+    #doclist = list(newsStr.split("|"))[0:doc_len]
+    doclist = list(newsStr.split("|"))
     f.close()
     cleaned_docs = []
     for doc in doclist:
@@ -68,18 +61,33 @@ def tf_idf(tokenized_docs):
     tfidf_terms = tfidf.get_feature_names()
     return tfidf, tfidf_matrix, tfidf_terms
 
-def apply_lda(topics, iteration, matrix, terms):
+def apply_lda(topics, iteration, wrd_per_grp , matrix, terms):
     #### Latent Dirichlet Allocation
     lda_model = LatentDirichletAllocation(n_components=topics , learning_method='online', random_state=777, max_iter=iteration)
     lda_top = lda_model.fit_transform(matrix)
 
     a = input("Do you want to see the topics and linked words? [y/n]")
-    if a == 'y': get_topics(lda_model.components_, terms);
-    return lda_model
 
+    if a == 'y':
+        for idx, topic in enumerate(lda_model.components_):
+            print("Topic %d :" % (idx+1), [(terms[i], topic[i].round(2)) for i in topic.argsort()[:-wrd_per_grp -1:-1]])
+'''
 def get_topics(components, feature_names, n=30):
     for idx, topic in enumerate(components):
         print("Topic %d :" % (idx+1), [(feature_names[i], topic[i].round(2)) for i in topic.argsort()[:-n -1:-1]])
+'''
+def gensim_lda(tokenized_docs, n_tpic=20, n_wrd=10):
+    dictionary = gensim.corpora.Dictionary(tokenized_docs)
+    corpus = [dictionary.doc2bow(text) for text in tokenized_docs]
+    lda_model = gensim.models.ldamodel.LdaModel(corpus, num_topics = n_tpic, id2word=dictionary)
+    topics = lda_model.print_topics(num_words=n_wrd)
+    for topic in topics: print(topic);
+    #### LDA 시각화
+    #a = input("Do you want to visualize result?[y/n] ")
+    #if a == 'y':
+        #pyLDAvis.enable_notebook()
+        #vis = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary)
+        #pyLDAvis.show(vis)#This could be done in jupyter notebook
 
 def apply_word2vec(tokenized_docs, keyword, size=100, window=2,min_count=10,iteration=100):
     w2vmodel = Word2Vec(sentences=tokenized_docs, min_count=min_count)
@@ -87,49 +95,17 @@ def apply_word2vec(tokenized_docs, keyword, size=100, window=2,min_count=10,iter
     return w2vmodel
 
 if __name__ == "__main__":
-    cleaned_docs = get_cleaned_text("뉴스텍스트.txt",5)
+    cleaned_docs = get_cleaned_text("통합텍스트.txt",-1)
     tokenized_docs = tokenization(cleaned_docs)
     #### df matrix로 lda
-    #dtm, dt_matrix, dt_terms = create_dtm(tokenized_docs, 3)
-    #lda_model = apply_lda(30, 20, dt_matrix, dt_terms)
-    #### tf-idf maxrix로 lda 그대로 쓰는것에 비해 오히려 관련도가 떨어짐
+    # dtm, dt_matrix, dt_terms = create_dtm(tokenized_docs, 3)
+    #apply_lda(100,50, 10, dt_matrix, dt_terms)
+
+    #### tf-idf maxrix로 lda  #####그대로 쓰는것에 비해 오히려 관련도가 떨어짐
     tfidf, tfidf_matrix, tfidf_terms = tf_idf(tokenized_docs)
-    lda_model = apply_lda(30, 20, tfidf_matrix, tfidf_terms)
+    apply_lda(100, 50, 10, tfidf_matrix, tfidf_terms)
 
-    #### LDA 시각화
-    pyLDAvis.enable_notebook()
-    vis = pyLDAvis.sklearn.prepare(lda_model, tfidf_matrix)
+    #gensim_lda(tokenized_docs, 500, 10)  #### for lda visualization and results is slightly different than scikitlearn LDA model
     #### 벡터화로 비슷한 단어 나타내기 tokenization 이후 Word2Vec
-    ## 이걸로 지표별 연관 단어 찾고
     apply_word2vec(tokenized_docs, "")
-
-
-######## 네트워크 분석 ###########################
-    result = list(apriori(tokenized_docs, min_support=0.01))
-    df = pd.DataFrame(result)
-    df['length'] = df['items'].apply(lambda x: len(x))
-    df = df[(df['length'] == 2) & (df['support'] >= 0.01)].sort_values(by='support', ascending=False)
-
-########### 여기서부터
-    # tfidf_dict = {}
-    # for idx, terms in enumerate(tfidf.get_feature_names()):
-    #     tfidf_dict[terms] = idx
-
-    # for i, sent in enumerate(detokenized_docs):
-    #     print('====== document[%d] ======' % i)
-    #     print( [ (token, sp_matrix[i, tfidf_dict[token]]) for token in sent.split() ] )
-
-    # matrix_array = tfidf_matrix.toarray()
-    # print(matrix_array.shape)
-
-########### 이거는 tf-idf가 해결 된 다음
-    lda_model = LatentDirichletAllocation(n_components=20 , learning_method='online', random_state=777, max_iter=10)
-    lda_top = lda_model.fit_transform(tfidf_matrix)
-
-    #print(lda_model.components_)
-    #print(lda_model.components_.shape)
-
-    terms = tfidf.get_feature_names()
-    print(terms)
-    get_topics(lda_model.components_, terms)
 
